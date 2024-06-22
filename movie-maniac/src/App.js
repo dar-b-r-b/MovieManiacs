5f8d09652ea72d7e5c2e5427acbd09590af0809e
dar-b-r-b <d.tausheva9@gmail.com>
2024-06-22 21:40:03 +0300

    add main layout

--- movie-maniac/src/App.js
+++ movie-maniac/src/App.js
@@ -1,13 +1,67 @@
+import "./App.css";
+const classNameForButtons =
+  "rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50";
 
-import './App.css';
+function Header() {
+  return (
+    <header className="flex justify-center">
+      <h1 className="mt-4 archivo-black-regular"> Movie Maniacs</h1>
+    </header>
+  );
+}
+
+function ButtonAddMovie() {
+  return (
+    <button type="button" className={classNameForButtons + " ml-32 mb-6"}>
+      Добавить фильм
+    </button>
+  );
+}
+
+function ButtonRandomMovie() {
+  return (
+    <button type="button" className={classNameForButtons + " mr-32 mb-6"}>
+      Рандомный фильм
+    </button>
+  );
+}
+
+function MoviesInformation() {
+  return (
+    <div className="flex mt-6 mr-28 ml-28 justify-evenly h-24">
+      <img alt="Обложка фильма" src="film-cover.jpg" className=""></img>
+      <div className="flex flex-wrap basis-72">
+        <p>Название: Non nisi sit commodo culpa quis.</p>
+        <p>Жанр: Genre, genre</p>
+      </div>
+      <p className="basis-72">
+        Reprehenderit do proident enim laboris sint sunt cupidatat incididunt
+        eu. Commodo consectetur officia enim.
+      </p>
+      <div className="flex flex-col items-center">
+        <img className="size-fit" alt="" src="icon-eye-50.png"></img>
+        <p>Просмотрено</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <img className="size-fit" alt="" src="icon-delete-50.png"></img>
+        <p>Удалить</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <header >
-
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="flex justify-evenly mt-4">
+        <ButtonAddMovie />
+        <ButtonRandomMovie />
+      </div>
+      <MoviesInformation />
+      <MoviesInformation />
+      <MoviesInformation />
+    </>
   );
 }
 
