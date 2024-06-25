80724d0e21c1cc90c29cc9b728fc4551d63b235e
dar-b-r-b <d.tausheva9@gmail.com>
2024-06-25 13:15:04 +0300

    added modal window

---
+++ movie-maniac/src/AddMovieForm.js
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+function UploadMovieCover() {
+  return (
+    <div>
+      <label
+        htmlFor="cover-photo"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Обложка
+      </label>
+      <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+        <div className="text-center">
+          <div className="mt-4 flex text-sm leading-6 text-gray-600">
+            <label
+              htmlFor="file-upload"
+              className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+            >
+              <span>Upload a file</span>
+              <input
+                id="file-upload"
+                name="file-upload"
+                type="file"
+                className="sr-only"
+              />
+            </label>
+            <p className="pl-1">or drag and drop</p>
+          </div>
+          <p className="text-xs leading-5 text-gray-600">
+            PNG, JPG, GIF up to 10MB
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function InputMovieTitle() {
+  return (
+    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div className="sm:col-span-3">
+        <label
+          htmlFor="movie-name"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Название
+        </label>
+        <div className="mt-2">
+          <input
+            type="text"
+            name="movie-name"
+            id="movie-name"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function InputMovieGenre() {
+  return (
+    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div className="sm:col-span-3">
+        <label
+          htmlFor="movie-genre"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Жанры
+        </label>
+        <div className="mt-2">
+          <input
+            type="text"
+            name="movie-genre"
+            id="movie-genre"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+function InputCommentAboutMovie() {
+  return (
+    <div className="mt-10">
+      <label
+        htmlFor="about"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Комментарий
+      </label>
+      <div className="mt-2">
+        <textarea
+          id="comment"
+          name="comment"
+          rows={2}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          defaultValue={""}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default function AddMovieForm() {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <Dialog className="relative z-10" open={isOpen} onClose={setIsOpen}>
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in md:block"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
+          <DialogPanel
+            transition
+            className="flex w-full transform text-left text-base transition data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in md:my-8 md:max-w-2xl md:px-4 data-[closed]:md:translate-y-0 data-[closed]:md:scale-95 lg:max-w-4xl"
+          >
+            <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+              <button
+                type="button"
+                className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8"
+                onClick={() => setIsOpen(false)}
+              >
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+
+              <div className="flex flex-col">
+                <UploadMovieCover />
+                <InputMovieTitle />
+                <InputMovieGenre />
+                <InputCommentAboutMovie />
+                <div className="mt-6 flex justify-end gap-x-6">
+                  <button
+                    type="button"
+                    className="text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Отмена
+                  </button>
+                  <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Сохранить
+                  </button>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
