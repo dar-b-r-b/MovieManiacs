cc636fbc5e832f5d90d0517800608f7196ad02f9
dar-b-r-b <d.tausheva9@gmail.com>
2024-06-26 12:49:57 +0300

    added pagination

--- movie-maniac/src/App.js
+++ movie-maniac/src/App.js
@@ -63,6 +63,48 @@ function App() {
       <MoviesInformation />
       <MoviesInformation />
       <AddMovieForm />
+      <div className="mt-6 flex justify-center">
+        <a
+          href="/#"
+          className="flex items-center justify-center px-2 h-8 me-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+        >
+          <svg
+            className="w-3.5 h-3.5 me-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 5H1m0 0 4 4M1 5l4-4"
+            />
+          </svg>
+        </a>
+        <a
+          href="/#"
+          className="flex items-center justify-center px-2 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+        >
+          <svg
+            className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
     </>
   );
 }
